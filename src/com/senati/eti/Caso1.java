2473b6ae4ea6c8d3176938a2f8e5c17790e7a3ae
USUARIO01 <USUARIO01@DESKTOP-6RNB03S>
2021-03-30 22:00:38 -0500

    Casos resueltos

---
+++ src/com/senati/eti/Caso1.java
@@ -0,0 +1,15 @@
+package com.senati.eti;
+
+import java.awt.dnd.DragSourceListener;
+
+public class Caso1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenido programador SENATINO");
+				
+
+
+	}
+
+}
